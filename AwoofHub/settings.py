2af64af0baff3a1218c862969059f235a0fb8760
mide-driod <Ayomighty002@gmail.com>
2026-06-22 13:51:29 +0100

    e-commerce

--- AwoofHub/settings.py
+++ AwoofHub/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from datetime import timedelta
 from decouple import config
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -88,6 +89,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+DATABASES = {
+    'default': dj_database_url.parse(config('DATABASE_URL'))
 }
 
 
